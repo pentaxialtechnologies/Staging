b1ff20131123207e70657cdd2fb5ac38773df1ad
pepentaxialtechnologies <pentaxialtechnologies@gmail.com>
2025-09-18 09:54:19 +0530

    bug resolved

--- src/app/api/auth/ticket/getbyId/[id]/route.ts
+++ src/app/api/auth/ticket/getbyId/[id]/route.ts
@@ -1,19 +1,41 @@
+// import dbConnect from "@/lib/Mongodb";
+// import { NextResponse } from "next/server";
+// import { Employer_Tickets } from "@/models/ticket/EmployersTickets";
+
+
+// export async function GET(req:Request,{params}:{params:{id:string}}){
+//     try{
+//         await dbConnect()
+//     const {id} = params
+//     const ticket = await Employer_Tickets.findById(id).populate('userId')
+//     if(!ticket){
+//         return NextResponse.json({message:'Ticket Not found'},{status:404})
+//     }
+//         return NextResponse.json({data:ticket},{status:200})
+//     }
+//     catch(error){
+//         return NextResponse.json({message:'Internal Server Error'},{status:500})
+//     }
+// }
+
 import dbConnect from "@/lib/Mongodb";
 import { NextResponse } from "next/server";
 import { Employer_Tickets } from "@/models/ticket/EmployersTickets";
 
+export async function GET(req: Request, context: any) {
+  try {
+    await dbConnect();
 
-export async function GET(req:Request,{params}:{params:{id:string}}){
-    try{
-        await dbConnect()
-    const {id} = params
-    const ticket = await Employer_Tickets.findById(id).populate('userId')
-    if(!ticket){
-        return NextResponse.json({message:'Ticket Not found'},{status:404})
+    const { id } = context.params; // context.params.id
+    const ticket = await Employer_Tickets.findById(id).populate("userId");
+
+    if (!ticket) {
+      return NextResponse.json({ message: "Ticket Not found" }, { status: 404 });
     }
-        return NextResponse.json({data:ticket},{status:200})
-    }
-    catch(error){
-        return NextResponse.json({message:'Internal Server Error'},{status:500})
-    }
+
+    return NextResponse.json({ data: ticket }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
