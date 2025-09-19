9132684845c03731b920502e817e38af304999bb
pepentaxialtechnologies <pentaxialtechnologies@gmail.com>
2025-09-19 15:25:02 +0530

    auth

--- src/app/api/auth/employer/userprofile/create/route.ts
+++ src/app/api/auth/employer/userprofile/create/route.ts
@@ -2,7 +2,7 @@ import { EmployerProfile } from "@/models/Employer/UserProfile";
 import dbConnect from "@/lib/Mongodb";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { authOptions } from "../../../../../../lib/authOptions";
 export async function POST(req: Request) {
   try {
      const session = await getServerSession(authOptions);
